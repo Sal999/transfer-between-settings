70a4e5c899f08d527d9b8f2ff6f6962c8531b106
Sal999 <54933375+Sal999@users.noreply.github.com>
2019-11-04 22:43:36 +0100

    Add files via upload

---
+++ DataCleaning.R
@@ -0,0 +1,16 @@
+#' DataCleaning
+#'
+#' Replaces certiain entries, using the function MyReplace.
+#' @param df Dataframe. The study sample. No default.
+DataCleaning <- function(df) {
+  
+    ## Error handling
+    if (!is.data.frame(df))
+        stop ("Input has to be a data.frame")
+    ## Entry replacements
+    df$sex <- MyReplace(df$sex, 1, "Male")
+    df$sex <- MyReplace(df$sex, 2, "Female")
+    df$m30d <- MyReplace(df$m30d, 1, "Yes")
+    df$m30d <- MyReplace(df$m30d, 2, "No")
+    return(df)
+}
